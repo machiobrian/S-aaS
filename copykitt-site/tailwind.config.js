80989f691259dff218897137593f80f0c1bc024f
machiobrian <machiobrian29@gmail.com>
2022-10-24 01:17:53 +0300

    added the tailwind module

---
+++ copykitt-site/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// content: tells us where our content is so as to know when to refresh
+//  the tailwind css compilatio
